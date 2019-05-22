d4aa2c55a18cfab9202762e42bd61b71cbb9359c
Bozzi96 <37588821+Bozzi96@users.noreply.github.com>
2019-05-22 18:05:38 +0200

    Second part of the project done
    Checked stability, controllability and observability.
    TODO: Verify initial value theorem of the impulse response, for k=1

--- Exam/bogliolo_bozzi.m
+++ Exam/bogliolo_bozzi.m
@@ -21,6 +21,61 @@ zpk_k1 = zpk(sys_k1);
 zpk_k32 = zpk(sys_k32);
 zpk_k2 = zpk(sys_k2);
 
-
 %% Verifico stabilità, controllabilità, osservabilità per ognuno di essi
-%%%%%%%%%TODO
+
+% k=1
+fprintf("k=1\n\t");
+Ak1 = sys_k1.A; Bk1 = sys_k1.B; Ck1 = sys_k1.C;
+stab_k1 = isstable(sys_k1);
+poles_k1 = eig(Ak1);
+contr_k1 = rank(ctrb(Ak1,Bk1)) == max(size(Ak1));
+obsv_k1 = rank(obsv(Ak1,Ck1)) == max(size(Ak1));
+if stab_k1
+    fprintf("Sistema stabile\n\t");
+end
+if contr_k1
+    fprintf("Sistema controllabile\n\t");
+end
+if obsv_k1
+    fprintf("Sistema osservabile\n");
+end
+% k=3/2
+fprintf("k=3/2\n\t");
+Ak32 = sys_k32.A; Bk32 = sys_k32.B; Ck32 = sys_k32.C;
+stab_k32 = isstable(sys_k32);
+poles_k32 = eig(Ak32);
+contr_k32 = rank(ctrb(Ak32,Bk32)) == max(size(Ak32));
+obsv_k32 = rank(obsv(Ak32,Ck32)) == max(size(Ak32));
+if stab_k32
+    fprintf("Sistema stabile\n\t");
+end
+if contr_k32
+    fprintf("Sistema controllabile\n\t");
+end
+if obsv_k32
+    fprintf("Sistema osservabile\n");
+end
+
+% k=2;
+fprintf("k=2\n\t");
+Ak2 = sys_k2.A; Bk2 = sys_k2.B; Ck2 = sys_k2.C;
+stab_k2 = isstable(sys_k2);
+poles_k2 = eig(Ak2);
+contr_k2 = rank(ctrb(Ak2,Bk2)) == max(size(Ak2));
+obsv_k2 = rank(obsv(Ak2,Ck2)) == max(size(Ak2));
+if stab_k2
+    fprintf("Sistema stabile\n\t");
+end
+if contr_k2
+    fprintf("Sistema controllabile\n\t");
+end
+if obsv_k2
+    fprintf("Sistema osservabile\n");
+end
+
+%% Per k=1 plotto risposta all'impulso e verifico la validità del teorema iniziale
+
+impulse(sys_k1);
+
+
+
